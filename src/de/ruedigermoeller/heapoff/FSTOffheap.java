1f0cb2af103bd60ddd2d9c310c3473cf7306159e
moru0011 <moru0011@gmail.com>
2012-12-12 20:49:41 +0000

    map fix, removed block mode from input

--- src/de/ruedigermoeller/heapoff/FSTOffheap.java
+++ src/de/ruedigermoeller/heapoff/FSTOffheap.java
@@ -13,6 +13,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 import javax.swing.*;
 import java.io.IOException;
 import java.io.Serializable;
+import java.lang.reflect.Field;
 import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.util.Iterator;
@@ -46,7 +47,13 @@ public class FSTOffheap {
     FSTConfiguration conf = FSTConfiguration.createDefaultConfiguration();
     FSTObjectOutput out;
     FSTObjectInput in;
-    int lastPosition = 0;
+    int lastPosition = 0, currPosition=0;
+    FSTObjectInput.ConditionalCallback alwaysSkip = new FSTObjectInput.ConditionalCallback() {
+        @Override
+        public boolean shouldSkip(Object halfDecoded, int streamPosition, Field field) {
+            return true;
+        }
+    };
 
     public FSTOffheap(int size) throws IOException {
         buffer = ByteBuffer.allocateDirect(size*1000*1000);
@@ -55,7 +62,8 @@ public class FSTOffheap {
     }
 
     public int put(Object o, Object tag) throws IOException {
-        int res = buffer.position();
+        buffer.position(currPosition);
+        int res = currPosition;
         entry.content = o;
         entry.prevPosition = lastPosition;
         entry.content = o;
@@ -64,6 +72,7 @@ public class FSTOffheap {
         out.resetForReUse(null);
         out.writeObject(entry,entry.getClass());
         buffer.put(out.getBuffer(),0,out.getWritten());
+        currPosition = buffer.position();
         return res;
     }
 
@@ -71,8 +80,18 @@ public class FSTOffheap {
         return null;
     }
 
+    public Iterator iterator() {
+        return new OffHeapIterator(lastPosition);
+    }
+
     class OffHeapIterator implements Iterator {
+        byte tmp[] = new byte[100];
         int position;
+
+        OffHeapIterator(int position) {
+            this.position = position;
+        }
+
         @Override
         public boolean hasNext() {
             return position > 0;
@@ -80,7 +99,23 @@ public class FSTOffheap {
 
         @Override
         public Object next() {
-
+            in.setConditionalCallback(alwaysSkip);
+            buffer.position(position);
+            buffer.get(tmp);
+            try {
+                in.resetForReuse(tmp, 0, tmp.length);
+                ByteBufferEntry en = (ByteBufferEntry) in.readObject(ByteBufferEntry.class);
+                position = en.prevPosition;
+                return en.tag;
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (InstantiationException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
             return null;
         }
 
@@ -92,7 +127,7 @@ public class FSTOffheap {
 
     @Predict(ByteBufferEntry.class)
     static class ByteBufferEntry implements Serializable {
-        @Plain int prevPosition;
+        int prevPosition;
         @Conditional Object content;
         Object tag;
     }
@@ -104,10 +139,22 @@ public class FSTOffheap {
         Trader t = Trader.generateTrader(101,false);
         int siz = FSTConfiguration.createDefaultConfiguration().calcObjectSizeBytesNotAUtility(t);
         System.out.println("size "+siz);
+        int i1 = 90000;
+        System.out.println("size "+(siz*i1)/1000000+"mb");
         long tim = System.currentTimeMillis();
-        for ( int i = 0; i < 1000; i++ ) {
+        for ( int i = 0; i < i1; i++ ) {
             location = off.put(t,"hallo"+i);
         }
         System.out.println("TIM "+(System.currentTimeMillis()-tim));
+        tim = System.currentTimeMillis();
+        Iterator it = off.iterator();
+        while( it.hasNext() ) {
+            Object tag = it.next();
+        }
+        System.out.println("TIMITER "+(System.currentTimeMillis()-tim));
+//        it = off.iterator();
+//        while( it.hasNext() ) {
+//            System.out.println(it.next());
+//        }
     }
 }
