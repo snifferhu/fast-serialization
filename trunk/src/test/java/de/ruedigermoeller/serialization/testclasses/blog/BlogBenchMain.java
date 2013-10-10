8c6ca2812241e81d4f0eea875188bb7116a0640b
moru0011@gmail.com <moru0011@gmail.com@845bef77-3671-6759-e204-900723c8934e>
2013-10-10 19:21:21 +0000

    1.26 perf degradation fix

---
+++ trunk/src/test/java/de/ruedigermoeller/serialization/testclasses/blog/BlogBenchMain.java
@@ -0,0 +1,113 @@
+package de.ruedigermoeller.serialization.testclasses.blog;
+
+import de.ruedigermoeller.serialization.FSTObjectInput;
+import de.ruedigermoeller.serialization.FSTObjectOutput;
+
+import java.io.*;
+
+/**
+ * Copyright (c) 2012, Ruediger Moeller. All rights reserved.
+ * <p/>
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * <p/>
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ * <p/>
+ * Date: 10.10.13
+ * Time: 19:48
+ * To change this template use File | Settings | File Templates.
+ */
+public class BlogBenchMain {
+
+    public ByteArrayOutputStream bout = new ByteArrayOutputStream(100000);
+    ByteArrayInputStream bin;
+    public long timWrite;
+    public long timRead;
+    Object testObject;
+    int length;
+    private int type = 0; // 0 - fst 1 - JDK
+    int iterations;
+
+    public BlogBenchMain(int type, int iterations) {
+        this.type = type;
+        this.iterations = iterations;
+    }
+
+    public void run( Object toWrite ) throws Exception {
+        testObject = toWrite;
+
+        System.gc(); // clean heap
+        System.out.println("write ..");
+        long startTim = System.currentTimeMillis();
+        for ( int i = 0; i < iterations; i++ ) {
+            bout.reset(); // very cheap
+            if ( type == 0)
+                writeTest(new FSTObjectOutput(bout), toWrite );
+            else
+                writeTest(new ObjectOutputStream(bout), toWrite);
+        }
+        timWrite = System.currentTimeMillis()-startTim;
+        byte[] bytes = bout.toByteArray();
+        length = bytes.length;
+        bin = new ByteArrayInputStream(bytes);
+
+        System.gc(); // clean heap
+        System.out.println("read ..");
+        startTim = System.currentTimeMillis();
+        for ( int i = 0; i < iterations; i++ ) {
+            if ( type == 0) {
+                FSTObjectInput in = new FSTObjectInput(bin);
+                readTest(in);
+            }
+            else
+                readTest(new ObjectInputStream(bin));
+            bin.reset();
+        }
+        timRead = System.currentTimeMillis()-startTim;
+    }
+
+    public void dumpRes(String type) {
+        System.out.println(type+"  Size:" + length + ",  TimeRead: " + (timRead * 1000 * 1000 / iterations) + " nanoseconds,   TimeWrite: " + (timWrite * 1000 * 1000 / iterations) + " nanoseconds");
+    }
+
+    protected void readTest(ObjectInput in) throws Exception {
+        in.readObject();
+        in.close();
+    }
+
+    protected void writeTest(ObjectOutput out, Object toWrite) throws Exception {
+        out.writeObject(toWrite);
+        out.close();
+    }
+
+    public static void main(String args[]) throws Exception {
+
+        int iterations = 1000000;
+        Object test = new BlogBench();
+
+        BlogBenchMain fst = new BlogBenchMain(0,iterations);
+        BlogBenchMain jdk = new BlogBenchMain(1,iterations);
+
+        // warm up
+        jdk.run(test);
+        fst.run(test);
+
+        //test
+        fst.run(test);
+        fst.dumpRes("FST");
+
+        jdk.run(test);
+        jdk.dumpRes("JDK");
+
+    }
+}
