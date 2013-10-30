4f2812257c6d6eea94bfa836bfad0d195ebe0f72
moru0011 <moru0011@gmail.com>
2013-10-30 19:23:30 +0000

    wechdamit

--- fstgen/PyTest.py
+++
@@ -1,9 +0,0 @@
-import io
-import MyFSTFactory
-
-__author__ = 'ruedi'
-
-fac = MyFSTFactory.MyFSTFactory()
-
-stream = io.FileIO('\\tmp\\crosstest.oos')
-print( 'pok %s' % fac.decodeFromStream(stream) )
